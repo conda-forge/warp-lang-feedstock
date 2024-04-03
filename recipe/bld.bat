71e140f9f7273a1ab9b988a7305d058922baa705
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-04-03 14:27:49 +0200

    Cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ if not "%CUDA_COMPILER_VERSION%" == "None" (
     set "NVCC_APPEND_FLAGS=%NVCC_APPEND_FLAGS% --use-local-env"
     set "CUDA_COMPILER_VERSION=%cuda_compiler_version%"
     set "CUTLASS_PATH=%CD%"
-    )
+    set "CUDA_LIB_PATH=%CUDA_HOME%\lib\x64"
     set "ARGS=--cuda_path=%CUDA_HOME%"
 )
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,6 @@ if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
     export CUTLASS_PATH=$(pwd)
       if [[ ${cuda_compiler_version} == 12* ]]; then
         export CUDA_HOME="${BUILD_PREFIX}/targets/x86_64-linux"
-        export PATH=$PATH:${BUILD_PREFIX}/nvvm/bin
         export CUDA_TOOLKIT_INCLUDE_DIR="${PREFIX}/targets/x86_64-linux"
         export CUDA_INSTALL_PATH=$(which nvcc | awk -F'/bin/nvcc' '{print $1}')
     fi
