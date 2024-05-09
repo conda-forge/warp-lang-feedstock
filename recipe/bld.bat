22f62604a407ef10fd32d6f59d2d001d79c7c87c
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-05-09 14:18:20 +0200

    Update bld.bat
    Co-authored-by: Silvio Traversaro <silvio.traversaro@iit.it>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ set "ARGS="
 
 if not "%CUDA_COMPILER_VERSION%" == "None" (
     set "NVCC_APPEND_FLAGS=%NVCC_APPEND_FLAGS% --use-local-env"
-    set "ARGS=--cuda_path=\"%CUDA_HOME%\""
+    set ARGS=--cuda_path="%CUDA_HOME%"
     if "%CUDA_COMPILER_VERSION%" == "12.0" (
         set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v%CUDA_COMPILER_VERSION%
         set CUDA_BIN_PATH=%CUDA_PATH%\bin
