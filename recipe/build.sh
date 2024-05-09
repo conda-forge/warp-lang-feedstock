6d180a180f83af963aafb35775db23a6562864ef
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-05-09 16:16:53 +0200

    Fix indentation error

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,10 @@ set -exo pipefail
 
 if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
     export CUTLASS_PATH=$(pwd)
-      if [[ ${cuda_compiler_version} == 12* ]]; then
-        export CUDA_HOME="${BUILD_PREFIX}/targets/x86_64-linux"
-        export CUDA_TOOLKIT_INCLUDE_DIR="${PREFIX}/targets/x86_64-linux"
-        export CUDA_INSTALL_PATH=$(which nvcc | awk -F'/bin/nvcc' '{print $1}')
+    if [[ ${cuda_compiler_version} == 12* ]]; then
+      export CUDA_HOME="${BUILD_PREFIX}/targets/x86_64-linux"
+      export CUDA_TOOLKIT_INCLUDE_DIR="${PREFIX}/targets/x86_64-linux"
+      export CUDA_INSTALL_PATH=$(which nvcc | awk -F'/bin/nvcc' '{print $1}')
     fi
 fi
 
