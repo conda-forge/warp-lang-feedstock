1a8a6e08b0d5a70a42db1f186ec82d0228cc3776
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-05-09 09:21:22 +0200

    Update `CUDA_TOOLKIT_INCLUDE_DIR`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,13 +4,11 @@ set "ARGS="
 
 if not "%CUDA_COMPILER_VERSION%" == "None" (
     set "NVCC_APPEND_FLAGS=%NVCC_APPEND_FLAGS% --use-local-env"
-    @REM set "CUDA_COMPILER_VERSION=%cuda_compiler_version%"
-    @REM set "CUTLASS_PATH=%CD%"
-    @REM set "CUDA_LIB_PATH=%CUDA_HOME%\lib\x64"
-    @REM set "ARGS=--cuda_path=\"%CUDA_HOME%\""
+    set "ARGS=--cuda_path=\"%CUDA_HOME%\""
     if "%CUDA_COMPILER_VERSION%" == "12.0" (
         set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v%CUDA_COMPILER_VERSION%
         set CUDA_BIN_PATH=%CUDA_PATH%\bin
+        set CUDA_TOOLKIT_INCLUDE_DIR=%PREFIX%\Library
     )
 )
 
