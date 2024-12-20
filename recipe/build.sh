3d8b049bd7f055e827e3ff475e2002c8f5db6420
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-12-20 10:56:36 +0000

    Add `$CONDA_PREFIX/targets/x86_64-linux/lib` to LD search path
    Co-authored-by: Silvio Traversaro <silvio.traversaro@iit.it>

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,9 @@ if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
       export CUDA_TOOLKIT_INCLUDE_DIR="${PREFIX}/targets/x86_64-linux"
       export CUDA_INSTALL_PATH=$(which nvcc | awk -F'/bin/nvcc' '{print $1}')
     fi
+    if [[ "$target_platform" == linux-* ]]; then
+      LD_EXTRA_FLAGS="$LD_EXTRA_FLAGS -L$CONDA_PREFIX/targets/x86_64-linux/lib"
+    fi
 fi
 
 ${PYTHON} build_lib.py ${ARGS:-}
