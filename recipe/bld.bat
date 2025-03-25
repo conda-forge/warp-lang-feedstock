f23e8d8814f457672c26e5817ef4a18f4f8824ea
Filippo Luca Ferretti <filippo.ferretti@iit.it>
2025-03-25 18:55:42 +0000

    Fix offsets and headers in patched

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ set "ARGS="
 
 if not "%CUDA_COMPILER_VERSION%" == "None" (
     set NVCC_APPEND_FLAGS=--use-local-env
-    set ARGS=--cuda_path=%CUDA_HOME%
+    set ARGS="--cuda_path=%CUDA_HOME%"
     if "%CUDA_COMPILER_VERSION:~0,3%" == "12." (
         set CUDA_PATH=%LIBRARY_PREFIX%
         set CUDA_BIN_PATH=%CUDA_PATH%\bin
