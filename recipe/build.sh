778ebe9cb9b3b2c6c204e6d305ffed7891950b0b
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-10-21 12:29:01 +0200

    Tentative cleanup include paths

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,17 @@
 set -exo pipefail
 
 if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
-    export CUTLASS_PATH=$(pwd)
-    export CUDA_PATH="${PREFIX}/targets/${target_name}"
+    # export CUTLASS_PATH=$(pwd)
+    # export CUDA_PATH="${PREFIX}/targets/${target_name}"
     export CUDA_HOME="${BUILD_PREFIX}/targets/x86_64-linux"
-    export CUDA_TOOLKIT_INCLUDE_DIR="${BUILD_PREFIX}/targets/x86_64-linux"
-    export CUDA_INSTALL_PATH=$(which nvcc | awk -F'/bin/nvcc' '{print $1}')
+    # export CPATH="${PREFIX}/targets/x86_64-linux"
+    # export CUDA_INSTALL_PATH=$(which nvcc | awk -F'/bin/nvcc' '{print $1}')
     export LIBMATHDX_HOME="${BUILD_PREFIX}"
-    ARGS="--cuda_path=${CUDA_HOME}"
     if [[ "$target_platform" == linux-* ]]; then
-      export LD_EXTRA_FLAGS="$LD_EXTRA_FLAGS -L$CONDA_PREFIX/targets/x86_64-linux/lib"
+      # Add CUDA library paths for conda-forge packages
+      export LIBRARY_PATH="${BUILD_PREFIX}/targets/x86_64-linux/lib:${LIBRARY_PATH}"
+    #   export LIBRARY_PATH="${PREFIX}/targets/x86_64-linux/lib:${BUILD_PREFIX}/targets/x86_64-linux/lib:${BUILD_PREFIX}/lib:${LIBRARY_PATH}"
+    #   export LD_EXTRA_FLAGS="$LD_EXTRA_FLAGS -L${PREFIX}/targets/x86_64-linux/lib -L${BUILD_PREFIX}/targets/x86_64-linux/lib -L${BUILD_PREFIX}/lib"
     fi
 fi
 
