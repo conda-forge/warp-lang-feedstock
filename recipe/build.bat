b843f44692088285932e6ec63874bc01a6a536af
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-12-03 15:15:55 +0100

    Skip building LLVM from source

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,13 +2,7 @@
 
 if not "%CUDA_COMPILER_VERSION%" == "None" (
     set NVCC_APPEND_FLAGS=--use-local-env
-    set "CUDA_VERSION_CHECK=0"
-    if "%CUDA_COMPILER_VERSION:~0,3%" == "12." set "CUDA_VERSION_CHECK=1"
-    if "%CUDA_COMPILER_VERSION:~0,3%" == "13." set "CUDA_VERSION_CHECK=1"
-    if "%CUDA_VERSION_CHECK%" == "1" (
-        set CUDA_HOME=%CONDA_PREFIX%/Library/
-        set LIBMATHDX_HOME=%BUILD_PREFIX%
-    )
+    set LIBMATHDX_HOME=%BUILD_PREFIX%
     set "LIB=%CONDA_PREFIX%\Library\lib;%LIB%"
 )
 
@@ -17,7 +11,7 @@ set PM_PYTHON_EXT=%PYTHON%
 
 set
 
-%PYTHON% build_lib.py %ARGS% --quick -j%CPU_COUNT%
+%PYTHON% build_lib.py %ARGS% --quick -j%CPU_COUNT% --llvm_path=%BUILD_PREFIX% --cuda_path=%CONDA_PREFIX%/Library
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation --no-cache-dir
 if errorlevel 1 exit 1
