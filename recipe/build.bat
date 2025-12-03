6168ce997865146e6787eedd54403bda5716419f
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-12-03 15:44:09 +0100

    Force skipping CUDA support on non-CUDA builds

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,6 +4,7 @@ if not "%CUDA_COMPILER_VERSION%" == "None" (
     set NVCC_APPEND_FLAGS=--use-local-env
     set LIBMATHDX_HOME=%BUILD_PREFIX%
     set "LIB=%CONDA_PREFIX%\Library\lib;%LIB%"
+    set ARGS=--cuda_path=%CONDA_PREFIX%/Library
 )
 
 @REM Force packman to use system python
@@ -11,7 +12,7 @@ set PM_PYTHON_EXT=%PYTHON%
 
 set
 
-%PYTHON% build_lib.py %ARGS% --quick -j%CPU_COUNT% --llvm_path=%BUILD_PREFIX% --cuda_path=%CONDA_PREFIX%/Library
+%PYTHON% build_lib.py %ARGS% --quick -j%CPU_COUNT% --llvm_path=%BUILD_PREFIX%
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation --no-cache-dir
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -exo pipefail
 if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
     export LIBMATHDX_HOME="${BUILD_PREFIX}"
     export LIBRARY_PATH="${BUILD_PREFIX}/targets/x86_64-linux/lib:${LIBRARY_PATH}"
+    export ARGS="--cuda_path $BUILD_PREFIX/targets/x86_64-linux"
 fi
 
 # Make sure nvvm tools are found by nvcc
@@ -16,6 +17,6 @@ if [[ "${target_platform}" == linux-* ]]; then
     export PM_PYTHON_EXT="${PYTHON}"
 fi
 
-${PYTHON} build_lib.py ${ARGS:-} --cuda_path $BUILD_PREFIX/targets/x86_64-linux --llvm_path ${BUILD_PREFIX} --quick -j${CPU_COUNT} --use_dynamic_cuda
+${PYTHON} build_lib.py ${ARGS:-} --llvm_path ${BUILD_PREFIX} --quick -j${CPU_COUNT} --use_dynamic_cuda
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation --no-cache-dir
