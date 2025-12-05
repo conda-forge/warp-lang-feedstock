867a0f38b0d71222404f0059f7ebadc917b67926
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-12-05 16:43:33 +0100

    Avoid conda-forge LLVM for the time being
    This requires major changes in the codebase

--- recipe/build.bat
+++ recipe/build.bat
@@ -12,7 +12,7 @@ set PM_PYTHON_EXT=%PYTHON%
 
 set
 
-%PYTHON% build_lib.py %ARGS% --quick -j%CPU_COUNT% --llvm_path=%BUILD_PREFIX%/Library
+%PYTHON% build_lib.py %ARGS% --quick -j%CPU_COUNT%  --use_dynamic_cuda
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation --no-cache-dir
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,8 @@ set -exo pipefail
 if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
     export LIBMATHDX_HOME="${BUILD_PREFIX}"
     export LIBRARY_PATH="${BUILD_PREFIX}/targets/x86_64-linux/lib:${LIBRARY_PATH}"
-    export ARGS="--cuda_path $BUILD_PREFIX/targets/x86_64-linux"
+    export ARGS="--cuda_path ${BUILD_PREFIX}/targets/x86_64-linux"
+    export NVCC_APPEND_FLAGS="-I${BUILD_PREFIX}/include/cccl -I${BUILD_PREFIX}/targets/x86_64-linux/include"
 fi
 
 # Make sure nvvm tools are found by nvcc
@@ -17,6 +18,6 @@ if [[ "${target_platform}" == linux-* ]]; then
     export PM_PYTHON_EXT="${PYTHON}"
 fi
 
-${PYTHON} build_lib.py ${ARGS:-} --llvm_path ${BUILD_PREFIX} --quick -j${CPU_COUNT} --use_dynamic_cuda
+${PYTHON} build_lib.py ${ARGS:-} --quick -j${CPU_COUNT} --use_dynamic_cuda
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation --no-cache-dir
